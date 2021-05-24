b95dae241b0d8d57c33a564836c22c8b97a05580
Alexandr <31178187+calcq@users.noreply.github.com>
2021-05-24 23:14:41 +0300

    procedure done

--- src/Procedures.sql
+++ src/Procedures.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE count_students_in_group(code VARCHAR(30))
 BEGIN
 
 SELECT COUNT(Студент.Зачетка) FROM Студент
-INNER JOIN Группа ON Студент.Группа_id = Группа.id
+INNER JOIN Группа ON Студент.Группа_id = Группа.id;
 
 END//
 
@@ -21,6 +21,22 @@ BEGIN
 
 SELECT Студент.Зачетка, Студент.Фамилия, Студент.Имя, Группа.Зачетка FROM Студент
 RIGHT JOIN Группа ON Студент.Группа_id = Группа.id
-WHERE Студент.Зачетка = Группа.Староста_зачетка
+WHERE Студент.Зачетка = Группа.Староста_зачетка;
+
+END//
+
+-- Вывести колличество пар по предмету
+
+DELIMITER //
+
+CREATE PROCEDURE count_lessons(lesson_name VARCHAR(100))
+
+BEGIN
+
+SELECT Дисциплина.Название, Тип_занятия.Название, COUNT(1) FROM Занятие 
+JOIN План_дисциплин ON План_дисциплин.id = Занятие.План_дисциплин_id 
+JOIN Дисциплина ON Дисциплина.id = План_дисциплин.Дисциплина_id 
+JOIN Тип_занятия ON Тип_занятия.id = План_дисциплин.Тип_занятия_id 
+GROUP BY Дисциплина.Название, Тип_занятия.Название HAVING Дисциплина.Название = lesson_name;
 
 END//
